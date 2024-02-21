e2bf7600d696bdb2a18c690bdbe899e61ed01922
Nada-123 <nada.alshakiry@gmail.com>
2024-02-21 18:25:07 +0300

    Mission Accomplished

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solutions in this file
+
+function writeCards(array) {
+    let thankyouArray = array.map(item => `Thank you, ${item}, for the wonderful surprise gift!`);
+    return thankyouArray;
+}
+
+function countDown(num) {
+	for (let i = num; i >= 0; i--) {
+		console.log(i);
+	}
+}
